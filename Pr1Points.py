acb980145ee1d41feb03aabdb49b7870ffa972de
Mihail Petrov <mr.mihail.petrov@gmail.com>
2015-11-10 22:07:45 +0200

    addressed a bug in y coordinates

--- Pr1Points.py
+++ Pr1Points.py
@@ -5,15 +5,19 @@ inputLineOne = input()
 
 lineOneSPlit = inputLineOne.split(' ')
 
-x = int(lineOneSPlit[2][1:(len(lineOneSPlit[2])-1)])
+x = lineOneSPlit[2][1:len(lineOneSPlit[2])-1]
+x = int(x)
 # if we keep the syntax as specified x would be the [2] component if we split the string
 # from there we take the length of the string omitting the first and the last symbol
 # the result is then caseted to int
-y = int(lineOneSPlit[3][0:(len(lineOneSPlit[2])-2)])
+y = lineOneSPlit[3][0:len(lineOneSPlit[2])-2]
+y = int(y)
 # same as for x (ref above)
 
 cipher = input()
 # the sequence of the symbols is provided on a separate line as specified
+
+
 
 countWarps = 0
 # count the occurences of the '~' symbol
